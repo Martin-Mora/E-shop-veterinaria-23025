7d5643842f14784883c88ca1d6de8c9aa0250d5f
Martin Mora <martin_mora1209@hotmail.com>
2023-04-20 18:41:13 -0300

    Seccion de productos agregado/cambios en css/seccion servicios

--- main.js
+++ main.js
@@ -2,33 +2,24 @@ const hamburger= document.querySelector('.hamburger');
 const navOcult= document.querySelector('.navOcult');
 const navHidden = document.querySelector('.x');
 const containerCards = document.querySelector('.container-flex');
+const modal_background= document.querySelector('.modal-background');
 
 
+// menu responsive
 hamburger.addEventListener('click',()=>{
   navOcult.classList.toggle('navOcult')
+  modal_background.style.display = 'block';
 })
 
 navHidden.addEventListener('click',()=>{
   navOcult.classList.toggle('navOcult')
+  modal_background.style.display = 'none';
+})
+
+// modal
+modal_background.addEventListener('click',()=>{
+  modal_background.style.display = 'none';
+  navOcult.classList.toggle('navOcult');
 })
 
 
-// function pintarCards(arrayData){
-//   let card='';
- 
-//   for(let i=0; i<arrayData.length;i++ ){
-//     card+=`<div class="card">
-//     <img src="${arrayData[i].image}" class="card-img-top" alt="...">
-//     <div class="card-body">
-//       <h5 class="card-title">Card title</h5>
-//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//       <a href="#" class="btn">Go somewhere</a>
-//     </div>
-//   </div>`
-//   }
-
-//   containerCards.innerHTML=card;
-// }
-
-
-// pintarCards(data.products)
---
+++ productos.js
@@ -0,0 +1,154 @@
+const container=document.querySelector('.productos__grid');
+const selectProductos= document.querySelector('.productos');
+
+
+
+
+renderCards();
+
+
+//filtros
+selectProductos.addEventListener('change',(e)=>{
+  console.log(e.target.value) ;
+
+  if(e.target.value=="producto"){
+    renderCards();
+    }
+
+  if(e.target.value=="comida gato"){
+  mostrarComidaGato();
+  }
+
+  if(e.target.value=="comida perro"){
+    mostrarComidaPerro();
+    }
+
+  if(e.target.value=="juguetes"){
+    mostrarJuguetes();
+    }
+  
+})
+
+
+
+
+
+
+// funciones
+
+function renderCards(){
+  let card='';
+  let productos=data.products
+
+  productos.forEach(element=> {
+    card+=` <div class="card">
+    <div class="card__img">
+      <img src="${element.image}" alt="">
+    </div>
+
+    <div class="description">
+      <h5 class="description__title">${element.name} </h5>
+      <p class="description__card">${element.description}</p>
+      ${element.kg ? `<p class="description__kg">${element.kg}</p>` : ''}
+      <p class="description__price"> <span class="precio">Precio:</span> $${element.price}</p>
+      <a href="#" class="description__btn">COMPRAR</a>
+    </div>
+  </div>
+  `
+  } )
+
+  container.innerHTML=card;
+}
+
+
+
+function mostrarComidaGato(){
+  let card='';
+  let productos=data.products
+
+  productos.forEach(element=> {
+
+    if(element.category=="comida gato"){
+
+      card+=` <div class="card">
+      <div class="card__img">
+        <img src="${element.image}" alt="">
+      </div>
+  
+      <div class="description">
+        <h5 class="description__title">${element.name}</h5>
+        <p class="description__card">${element.description}</p>
+        ${element.kg ? `<p class="description__kg">${element.kg}</p>` : ''}
+        <p class="description__price"> <span class="precio">Precio:</span> $${element.price}</p>
+        <a href="#" class="description__btn">COMPRAR</a>
+      </div>
+    </div>
+    `
+    }
+
+  } )
+
+  container.innerHTML=card;
+}
+
+
+function mostrarComidaPerro(){
+  let card='';
+  let productos=data.products
+
+  productos.forEach(element=> {
+
+    if(element.category=="comida perro"){
+
+      card+=` <div class="card">
+      <div class="card__img">
+        <img src="${element.image}" alt="">
+      </div>
+  
+      <div class="description">
+        <h5 class="description__title">${element.name}</h5>
+        <p class="description__card">${element.description}</p>
+        ${element.kg ? `<p class="description__kg">${element.kg}</p>` : ''}
+        <p class="description__price"> <span class="precio">Precio:</span> $${element.price}</p>
+        <a href="#" class="description__btn">COMPRAR</a>
+      </div>
+    </div>
+    `
+    }
+
+  } )
+
+  container.innerHTML=card;
+}
+
+
+function mostrarJuguetes(){
+  let card='';
+  let productos=data.products
+
+  productos.forEach(element=> {
+
+    if(element.category=="juguetes"){
+
+      card+=` <div class="card">
+      <div class="card__img">
+        <img src="${element.image}" alt="">
+      </div>
+  
+      <div class="description">
+        <h5 class="description__title">${element.name}</h5>
+        <p class="description__card">${element.description}</p>
+        ${element.kg ? `<p class="description__kg">${element.kg}</p>` : ''}
+        <p class="description__price"> <span class="precio">Precio:</span> $${element.price}</p>
+        <a href="#" class="description__btn">COMPRAR</a>
+      </div>
+    </div>
+    `
+    }
+
+  } )
+
+  container.innerHTML=card;
+}
+
+
