e42f00456c0523c41216f886179650b0484f94cb
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2023-04-23 02:13:41 -0300

    agregado productos destacados

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const FILA=document.querySelector('.inicio-contenedor-carrusel-ofertas');
 const FLECHA_IZQUIERDA=document.querySelector('.inicio-flecha-izquierda');
 const FLECHA_DERECHA=document.querySelector('.inicio-flecha-derecha');
 
-// Mover las imágenes con la flecha derecha //
+const FILA_DESTACADOS=document.querySelector('.inicio-contenedor-carrusel-productos-destacados');
+const FLECHA_IZQUIERDA_DESTACADOS=document.getElementById('inicio-flecha-izquierda');
+const FLECHA_DERECHA_DESTACADOS=document.getElementById('inicio-flecha-derecha');
+
+// Mover las imágenes con la flecha derecha Ofertas//
 FLECHA_DERECHA.addEventListener('click', () => {
   FILA.scrollLeft= FILA.scrollLeft + FILA.offsetWidth;
 });
 // Mover las imágenes con la flecha izquierda //
 FLECHA_IZQUIERDA.addEventListener('click', () =>{
   FILA.scrollLeft= FILA.scrollLeft - FILA.offsetWidth;
+})
+
+// Mover las imágenes con la flecha derecha Productos destacados//
+FLECHA_DERECHA_DESTACADOS.addEventListener('click', () => {
+  FILA_DESTACADOS.scrollLeft= FILA_DESTACADOS.scrollLeft + FILA_DESTACADOS.offsetWidth;
+});
+// Mover las imágenes con la flecha izquierda //
+FLECHA_IZQUIERDA_DESTACADOS.addEventListener('click', () =>{
+  FILA_DESTACADOS.scrollLeft= FILA_DESTACADOS.scrollLeft - FILA_DESTACADOS.offsetWidth;
 })
